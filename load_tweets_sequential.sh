fa38dcf3458db5d8059741357ba56022c3a5a82e
Michaela Yeung <yeung.michaela@gmail.com>
2026-04-19 21:20:19 -0700

    edit code

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,19 +6,22 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1025 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    echo $file
+    unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1025/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    python3 load_tweets.py --db="postgresql://postgres:pass@localhost:1026" --inputs="$file"
+    echo $file
+    python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1026/postgres --inputs=$file
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1027/ --inputs $file
+    echo $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1027/postgres --inputs=$file
 done
